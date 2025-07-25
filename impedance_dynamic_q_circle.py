1a70ea52e420048a99083296021c0b7b3b18aee1
NANA-YANG-ZHI <zhijingshui.yang@gmail.com>
2025-07-25 13:01:02 +0200

    add dynamic q tracking as another file

--- impedance_dynamic_q_circle.py
+++ impedance_dynamic_q_circle.py
@@ -1,26 +1,29 @@
 # ------------------------------------------------------------------------------
-# Hybrid Force-Impedance Control for Fast End-Effector Motions
+# Impedance Control with PD Force Feedback — Circular Motion Task
 # ------------------------------------------------------------------------------
-# 
+# This implementation enhances impedance control by adding PD feedback in force space:
+#     F = F_desired + Kp * (F_desired - F_contact) + Kd * d/dt(F_error)
+#     τ_force = Jᵀ * F
+#
+# Notes:
+# - Requires accurate Jacobian and stable force estimation from simulation.
 # ------------------------------------------------------------------------------
 
 import mujoco
 import mujoco.viewer
 import numpy as np
 import time
-import pinocchio as pino
-
+from reference_position_tracking import fast_ik_jacobian_based
 import logging
 
 # Configure the logger
 logging.basicConfig(
-    filename='hybrid_force_log.txt',        # log file name
+    filename='baseline_force_log.txt',        # log file name
     filemode='w',                    # 'w' to overwrite each run, 'a' to append
     level=logging.INFO,              # set to DEBUG for more details
     format='%(asctime)s - %(message)s',
     datefmt='%H:%M:%S'
 )
-
 
 # Cartesian impedance control gains.
 impedance_pos = np.asarray([100.0, 100.0, 100.0])  # [N/m]
@@ -30,7 +33,7 @@ impedance_ori = np.asarray([50.0, 50.0, 50.0])  # [Nm/rad]
 Kp_null = np.asarray([75.0, 75.0, 50.0, 50.0, 40.0, 25.0, 25.0])
 
 # Damping ratio for both Cartesian and joint impedance control.
-damping_ratio = 0.7
+damping_ratio = 1.0
 
 # Gains for the twist computation. These should be between 0 and 1. 0 means no
 # movement, 1 means move the end-effector to the target in one integration step.
@@ -50,46 +53,8 @@ gravity_compensation: bool = True
 # Simulation timestep in seconds.
 dt: float = 0.002
 
-def dynamically_consistent_inv(jac, M_inv):
-    """
-    Compute dynamically consistent pseudoinverse
-    J^{M+} = M^{-1} J^T (J M^{-1} J^T)^{-1}
-    """
-    Mx_inv = jac @ M_inv @ jac.T
-    if abs(np.linalg.det(Mx_inv)) >= 1e-2:
-        Mx = np.linalg.inv(Mx_inv)
-    else:
-        Mx = np.linalg.pinv(Mx_inv, rcond=1e-2)
-    return M_inv @ jac.T @ Mx
-
-def hierarchical_impedance_jacob(jac_list: list, dim):
-    # draw circle: only 2 subspace jac can be defined,
-    # last one - null space, there is no jac, it has to be calculated
-    # if give full M, dynamical consistant inverse can be found - J_inv
-    # M_inv = dynamically_consistent_pinv(J_aug, M)
-    Ns = []
-    I = np.eye(dim)
-    J_aug = np.empty((0, dim))
-    Ns.append(I)
-    for i, jac in enumerate(jac_list):
-        J_aug = np.vstack([J_aug, jac_list[i]])
-        J_aug_inv = np.linalg.pinv(J_aug)
-        N = I - J_aug_inv @ J_aug
-        Ns.append(N)
-    # find null space J_null
-    U, s, Vt = np.linalg.svd(J_aug)
-    rank = np.sum(s > 1e-10)
-    J_null = Vt[rank:, :]
-    jac_list.append(J_null)
-    
-    J_bars = []
-    for N, jac in zip(Ns, jac_list):
-        J_bar = jac @ N.T
-        J_bars.append(J_bar)
-    return Ns, J_bars
-
 def check_world_ee_contact_force(data, model):
-    contact_force_world = np.zeros(6)
+    contact_force_world = np.zeros(3)
     if data.ncon > 0:
         # Compute the contact forces.
         contact_force_local = np.zeros(6)
@@ -99,20 +64,16 @@ def check_world_ee_contact_force(data, model):
                 mujoco.mj_contactForce(model, data, i, contact_force_local)
                 break
         contact_rot = contact.frame.reshape(3, 3) # from local to world
-        # TODO: can I get world frame moment like this?
-        contact_force_world[:3] = contact_rot @ contact_force_local[:3]
-        contact_force_world[3:] = contact_rot @ contact_force_local[3:]
+        contact_force_local = contact_force_local[:3]
+        contact_force_world = contact_rot @ contact_force_local
     return contact_force_world
 
 def main() -> None:
     assert mujoco.__version__ >= "3.1.0", "Please upgrade to mujoco 3.1.0 or later."
 
     # Load the model and data.
-    xml_path = "kuka_iiwa_14/scene_notarget.xml"
-    model = mujoco.MjModel.from_xml_path(xml_path)
+    model = mujoco.MjModel.from_xml_path("kuka_iiwa_14/scene_notarget.xml")
     data = mujoco.MjData(model)
-    pino_model = pino.buildModelFromMJCF(r"C:\wkspace\mj_ctrl\kuka_iiwa_14\iiwa14.xml")
-    pino_data = pino_model.createData()
 
     model.opt.timestep = dt
 
@@ -150,14 +111,6 @@ def main() -> None:
     
     target_pos = np.array([0.5, 0., 0.45])  # Note that the height of the table is 0.45m
     target_quat = np.array([0., 1., 0., 0.])
-    x_dot_desired = np.zeros(3)
-    x_ddot_desired = np.zeros(3)
-
-    # normal control
-    twist = np.zeros(6)
-    site_quat = np.zeros(4)
-    site_quat_conj = np.zeros(4)
-    error_quat = np.zeros(4)
 
     # Circle drawing parameters
     circle_center = np.array([0.5, 0.0, 0.45])  # Center of circle on table
@@ -168,11 +121,14 @@ def main() -> None:
     contact_threshold = 8.0  # Force threshold to start drawing (close to desired 10N)
     contact_stable_time = 0
     contact_stable_duration = 1.0
-    angular_speed = np.pi / 4
+    angular_speed = np.pi
 
     # Pre-allocate numpy arrays.
     jac = np.zeros((6, model.nv))
-    J_dot = np.zeros((6, model.nv))
+    twist = np.zeros(6)
+    site_quat = np.zeros(4)
+    site_quat_conj = np.zeros(4)
+    error_quat = np.zeros(4)
     M_inv = np.zeros((model.nv, model.nv))
     Mx = np.zeros((6, 6))
     
@@ -200,16 +156,9 @@ def main() -> None:
     desired_forces = []
     tau_forces = []
     taus = []
+    ee_positions = []
+    target_positions = []
 
-    # S_f and S_v are mappings between end effector force & verlocity and constraint frame force & verlocity
-    S_f = np.zeros((6, 3)) 
-    S_f[2, 0] = 1
-    S_f[3, 1] = 1
-    S_f[4, 2] = 1
-    S_v = np.zeros((6, 3))
-    S_v[0, 0] = 1
-    S_v[1, 1] = 1
-    S_v[5, 2] = 1
     with mujoco.viewer.launch_passive(
         model=model,
         data=data,
@@ -226,21 +175,21 @@ def main() -> None:
         # viewer.opt.frame = mujoco.mjtFrame.mjFRAME_SITE
         while viewer.is_running():
             step_start = time.time()
-            current_contact_force = check_world_ee_contact_force(data, model)
-            F_ext_phi = current_contact_force[2]
-            F_ext_x = current_contact_force @ S_v
-            F_ext_v = None # no external contact on the arm and elbows
+            world_frame_force = check_world_ee_contact_force(data, model)
+            current_contact_force = world_frame_force[2]
+            current_friction_force = world_frame_force[:2]
             # Check for stable contact to start drawing
-            if F_ext_phi > contact_threshold and not circle_drawing:
+            if current_contact_force > contact_threshold and not circle_drawing:
                 contact_stable_time += dt
                 if contact_stable_time >= contact_stable_duration:
                     circle_drawing = True
                     circle_start_time = data.time
                     print("Starting circle drawing!")
                     logging.info("Starting circle drawing!")
-            elif F_ext_phi <= contact_threshold:
+            elif current_contact_force <= contact_threshold:
                 contact_stable_time = 0
-
+            logging.info(f"Time: {step_start:.3f}, current_contact_force: {current_contact_force}")
+            
             if circle_drawing:
                 elapsed_time = data.time - circle_start_time
                 if elapsed_time < circle_duration:
@@ -249,153 +198,56 @@ def main() -> None:
                     target_pos[0] = circle_center[0] + circle_radius * np.cos(angle)
                     target_pos[1] = circle_center[1] + circle_radius * np.sin(angle)
                     target_pos[2] = circle_center[2]  # Keep Z at table height
-                    # x_dot
-                    x_dot_desired[0] = -circle_radius * angular_speed * np.sin(angle)
-                    x_dot_desired[1] =  circle_radius * angular_speed * np.cos(angle)
-                    x_dot_desired[2] = 0.0
-                    # x_ddot
-                    x_ddot_desired[0] = -circle_radius * angular_speed**2 * np.cos(angle)
-                    x_ddot_desired[1] = -circle_radius * angular_speed**2 * np.sin(angle)
-                    x_ddot_desired[2] = 0.0
                 else:
                     # Circle completed, stop drawing
                     circle_drawing = False
                     print("Circle drawing completed!")
-            #-----------------------------------------------------------------------
-            # Position Control
-            # if there is no contact, use baseline algo to move the ee to surface
-            #-----------------------------------------------------------------------
-            if not circle_drawing:
-                dx = target_pos - data.site(site_id).xpos
-                twist[:3] = Kpos * dx / integration_dt
-                mujoco.mju_mat2Quat(site_quat, data.site(site_id).xmat)
-                mujoco.mju_negQuat(site_quat_conj, site_quat)
-                mujoco.mju_mulQuat(error_quat, target_quat, site_quat_conj)
-                mujoco.mju_quat2Vel(twist[3:], error_quat, 1.0)
-                twist[3:] *= Kori / integration_dt
 
-                # Jacobian.
-                mujoco.mj_jacSite(model, data, jac[:3], jac[3:], site_id)
+            # record ee position and target pos
+            ee_positions.append(data.site(site_id).xpos.copy())
+            target_positions.append(target_pos.copy())
+            # control logic
+            dx = target_pos - data.site(site_id).xpos
+            twist[:3] = Kpos * dx / integration_dt
+            mujoco.mju_mat2Quat(site_quat, data.site(site_id).xmat)
+            mujoco.mju_negQuat(site_quat_conj, site_quat)
+            mujoco.mju_mulQuat(error_quat, target_quat, site_quat_conj)
+            mujoco.mju_quat2Vel(twist[3:], error_quat, 1.0)
+            twist[3:] *= Kori / integration_dt
 
-                # Compute the task-space inertia matrix.
-                mujoco.mj_solveM(model, data, M_inv, np.eye(model.nv))
-                Mx_inv = jac @ M_inv @ jac.T
-                if abs(np.linalg.det(Mx_inv)) >= 1e-2:
-                    Mx = np.linalg.inv(Mx_inv)
-                else:
-                    Mx = np.linalg.pinv(Mx_inv, rcond=1e-2)
+            # Jacobian.
+            mujoco.mj_jacSite(model, data, jac[:3], jac[3:], site_id)
+            logging.info(f"Time: {step_start:.3f}, jac: {jac}")
+            # Compute the task-space inertia matrix.
+            mujoco.mj_solveM(model, data, M_inv, np.eye(model.nv))
+            Mx_inv = jac @ M_inv @ jac.T
+            if abs(np.linalg.det(Mx_inv)) >= 1e-2:
+                Mx = np.linalg.inv(Mx_inv)
+            else:
+                Mx = np.linalg.pinv(Mx_inv, rcond=1e-2)
 
-                # Compute generalized forces.
-                tau = jac.T @ Mx @ (Kp * twist - Kd * (jac @ data.qvel[dof_ids]))
+            # Compute generalized forces.
+            tau = jac.T @ Mx @ (Kp * twist - Kd * (jac @ data.qvel[dof_ids]))
 
-                # Add joint task in nullspace.
-                # TODO: inverse kinematics to track q0 over time, so ee orientation can't be kept
-                Jbar = M_inv @ jac.T @ Mx
-                ddq = Kp_null * (q0 - data.qpos[dof_ids]) - Kd_null * data.qvel[dof_ids]
-                tau += (np.eye(model.nv) - jac.T @ Jbar.T) @ ddq
-
-                # Add gravity compensation.
-                if gravity_compensation:
-                    tau += data.qfrc_bias[dof_ids]
-
-            #--------------------------------------------------------
-            # Hybrid control for Force Control
-            #--------------------------------------------------------
+            # Add joint task in nullspace.
+            # TODO: inverse kinematics to track q0 over time, so ee orientation can't be kept
+            Jbar = M_inv @ jac.T @ Mx
+            
             if circle_drawing:
-                # ------------------------------------------------------
-                # Jacobians
-                # ------------------------------------------------------
-                mujoco.mj_jacSite(model, data, jac[:3], jac[3:], site_id)
-                logging.info(f"Time: {elapsed_time:.3f}, jac: {jac}")
-                # jac 
-                # J_phi = A @ jac
-                J_phi = S_f.T @ jac
-                J_motion = S_v.T @ jac
-                jac_1 = np.vstack([J_phi, J_motion]) # stacked phi and motion jacobi as one
-                # according to paper equation (9), only null space Jacobian needs to be derived
-                logging.info(f"Time: {elapsed_time:.3f}, J_phi: {J_phi}, J_motion: {J_motion}")
+                # Create temporary data for IK
+                data_temp = mujoco.MjData(model)
+                data_temp.qpos[:] = data.qpos.copy()
+                q_v = fast_ik_jacobian_based(model, data_temp, target_pos, target_quat, site_id)
+                ddq = Kp_null * (q_v - data.qpos[dof_ids]) - Kd_null * data.qvel[dof_ids]
+            else:
+                ddq = Kp_null * (q0 - data.qpos[dof_ids]) - Kd_null * data.qvel[dof_ids]
+            tau += (np.eye(model.nv) - jac.T @ Jbar.T) @ ddq
+            logging.info(f"Time: {step_start:.3f}, tau: {tau}")
 
-                
-                #----------------------------------------------------
-                # Null Space torque
-                #----------------------------------------------------
-                # Compute the task-space inertia matrix.
-                mujoco.mj_solveM(model, data, M_inv, np.eye(model.nv))
-                # dynamically consistent pseudoinverse
-                jac_1_inv = dynamically_consistent_inv(jac_1, M_inv)
-                N2 = np.eye(model.nv) - jac_1.T @ jac_1_inv.T
-                tau_ctrl_v = N2 @ (Kp_null * (q0 - data.qpos[dof_ids]) - Kd_null * data.qvel[dof_ids])
-                logging.info(f"Time: {elapsed_time:.3f}, tau_ctrl_v: {tau_ctrl_v}")
-                # # find null space J_null
-                # _, s, Vt = np.linalg.svd(jac_1)
-                # rank = np.sum(s > 1e-10)
-                # J_null = Vt[rank:, :]
-                # J_v = J_null @ N2.T
-
-                #---------------------------------------------------
-                # Motion Space
-                #----------------------------------------------------
-                # Compute the task-space inertia matrix for x-y plane
-                mujoco.mj_solveM(model, data, M_inv, np.eye(model.nv))
-                Mxy_inv = J_motion @ M_inv @ J_motion.T  # Now this will be 2x2
-                if abs(np.linalg.det(Mxy_inv)) >= 1e-2:
-                    Mxy = np.linalg.inv(Mxy_inv)
-                else:
-                    Mxy = np.linalg.pinv(Mxy_inv, rcond=1e-2)
-
-                twist[:3] = target_pos - data.site(site_id).xpos
-                mujoco.mju_mat2Quat(site_quat, data.site(site_id).xmat)
-                mujoco.mju_negQuat(site_quat_conj, site_quat)
-                mujoco.mju_mulQuat(error_quat, target_quat, site_quat_conj)
-                mujoco.mju_quat2Vel(twist[3:], error_quat, 1.0)
-                x_tilde = twist @ S_v
-                site_vel = jac @ data.qvel[dof_ids] #[vx, vy, vz, wx, wy, wz]
-                x_dot_tilde = (site_vel - np.concatenate([x_dot_desired, [0,0,0]])) @ S_v
-                # F_ctrl_x = (Mxy @ x_ddot_desired + 
-                #             C_x @ x_dot_desired - 
-                #             K_x @ x_tilde - 
-                #             D_x @ x_dot_tilde)
-                # TODO：task space coriolis matrix in motion space is complicated 
-                F_ctrl_x = (Mxy @ x_ddot_desired - 
-                            Kp @ S_v * x_tilde - 
-                            Kd @ S_v * x_dot_tilde)
-                logging.info(f"Time: {elapsed_time:.3f}, F_ctrl_x: {F_ctrl_x}")
-                # F_ctrl_x = (M_x @ x_ddot_desired + 
-                # C_x @ x_dot_desired - 
-                # K_x @ x_tilde - 
-                # D_x @ x_dot_tilde)
-                
-                #------------------------------------------------------
-                # Constraint space
-                #------------------------------------------------------
-                Mx_phi_inv = J_phi @ M_inv @ J_phi.T
-                if abs(np.linalg.det(Mx_phi_inv)) >= 1e-2:
-                    lambda_phi = np.linalg.inv(Mx_phi_inv)
-                else:
-                    lambda_phi = np.linalg.pinv(Mx_phi_inv, rcond=1e-2)
-                C = pino.computeCoriolisMatrix(pino_model, pino_data, data.qpos, data.qvel) 
-                F_desired_contact = np.array([-10.0, 0, 0])
-                # computeJointJacobiansTimeVariation
-                pino.computeJointJacobiansTimeVariation(pino_model, pino_data, data.qpos, data.qvel)
-                J_dot = pino.getFrameJacobianTimeVariation(pino_model, pino_data, site_id, pino.LOCAL_WORLD_ALIGNED)
-                # J_phi_dot = S_f @ J_dot
-                F_ctrl_constraint = (
-                    F_desired_contact -
-                    lambda_phi @ J_phi @ M_inv @ (J_motion.T @ F_ctrl_x + tau_ctrl_v) +
-                    lambda_phi @ J_phi @ M_inv @ (J_motion.T @ F_ext_x) 
-                )
-                # verlociy term: lambda_phi @ (J_phi @ M_inv @ C - J_phi_dot) @ data.qvel.copy()
-                logging.info(f"Time: {elapsed_time:.3f}, lambda_phi: {lambda_phi}, F_ext_x: {F_ext_x}")
-                #------------------------------
-                # Sum up all subspace
-                #------------------------------
-                tau = J_phi.T @ F_ctrl_constraint + J_motion.T @ F_ctrl_x + tau_ctrl_v
-                logging.info(f"Time: {elapsed_time:.3f}, phi_tau: {J_phi.T @ F_ctrl_constraint}, motion_tau: {J_motion.T @ F_ctrl_x}, null_tau: {tau_ctrl_v}")
-
-                # Add gravity compensation.
-                if gravity_compensation:
-                    tau += data.qfrc_bias[dof_ids]
-                    logging.info(f"Time: {elapsed_time:.3f}, g: {data.qfrc_bias[dof_ids]}")
+            # Add gravity compensation.
+            if gravity_compensation:
+                tau += data.qfrc_bias[dof_ids]
+            logging.info(f"Time: {step_start:.3f}, tau + g: {tau}")
 
             # Add force feedback.
             if force_feedback:
@@ -503,6 +355,28 @@ def main() -> None:
         legends = [f"Joint Torque {axs[i]}"]
         plt.legend(legends)
 
+    plt.show()
+
+    # ------------------------------------
+    # Plot end effector
+    # -----------------------------------
+    ee_positions = np.array(ee_positions)
+    target_positions = np.array(target_positions)
+    time_steps = np.arange(len(ee_positions)) * dt  # or use your timestamps array
+    # Create figure with 3 subplots
+    fig, axes = plt.subplots(3, 1, figsize=(10, 8))
+    # Plot X, Y, Z in separate subplots
+    axes_labels = ['X', 'Y', 'Z']
+    for i in range(3):
+        axes[i].plot(time_steps, ee_positions[:, i], 'b-', linewidth=2, label='End-Effector')
+        axes[i].plot(time_steps, target_positions[:, i], 'r--', linewidth=2, label='Target')
+        axes[i].set_ylabel(f'{axes_labels[i]} Position (m)')
+        axes[i].legend()
+        axes[i].grid(True, alpha=0.3)
+        axes[i].set_title(f'{axes_labels[i]} Position Tracking')
+    # Add x-label to bottom subplot
+    axes[2].set_xlabel('Time (s)')
+    plt.tight_layout()
     plt.show()
 
 if __name__ == "__main__":
