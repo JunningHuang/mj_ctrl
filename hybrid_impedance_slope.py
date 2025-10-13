change force local and world calculation and fix orientation of force plot

hybrid_impedance_slope.py
@@ -221,12 +221,12 @@ def main() -> None:
             step_start = time.time()
             reset_scene(scene, ngeom_init)
             if use_table:
-                current_contact_force, contact_pos = check_world_ee_contact_force(data, model)
+                current_force_world, current_force_local, contact_pos = check_world_ee_contact_force(data, model)
             else:
-                current_contact_force, contact_pos = check_world_ee_contact_force(data, model, obj_name='slope_geom')
+                current_force_world, current_force_local, contact_pos = check_world_ee_contact_force(data, model, obj_name='slope_geom')
             # TODO change size z
-            F_ext_phi = current_contact_force @ S_fc
-            F_ext_x = current_contact_force @ S_vc
+            F_ext_phi = current_force_local @ S_fc
+            F_ext_x = current_force_local @ S_vc
             F_ext_v = None # no external contact on the arm and elbows
             # Check if it arrives at surface
             if not circle_drawing:
@@ -402,11 +402,6 @@ def main() -> None:
                     control_force_compensation +
                     contact_force_compensation + verlociy_term
                 )
-                vis_forces = [
-                    np.concatenate([[0,0],F_desired_contact]), 
-                    np.concatenate([[0,0],control_force_compensation]),
-                    np.concatenate([[0,0],verlociy_term]),
-                    ]
                 # --------------------- PI term -------------------------------
                 # # F_ctrl_constraint = F_desired_contact.copy()
                 # pi_term, integral_force_error = PI_term(-F_ext_phi, F_desired_contact, dt, integral_force_error)
@@ -430,6 +425,11 @@ def main() -> None:
                 # tau += tau_ctrl_v
 
                 # Visualize the force command
+                vis_forces = [
+                    R_slope @ np.concatenate([[0, 0], F_desired_contact]),
+                    R_slope @ np.concatenate([[0, 0], control_force_compensation]),
+                    R_slope @ np.concatenate([[0, 0], verlociy_term]),
+                ]
                 positions = [
                     contact_pos + np.array([-0.03, 0.0, 0.0]),  # F_desired_contact (left)
                     contact_pos + np.array([0.0, 0.0, 0.0]),    # control_force_compensation (center)  
@@ -453,7 +453,7 @@ def main() -> None:
             #-----------------------------------------------------------------------
 
             # collect data for plotting
-            contact_forces.append(current_contact_force[:3])
+            contact_forces.append(current_force_local[:3])
             desired_forces.append(-F_desired_contact)
             ee_positions.append(data.site(site_id).xpos.copy())
             target_positions.append(target_pos.copy())

utils.py
@@ -148,6 +148,7 @@ def compute_ee_pose_error(target_pos, current_pos, target_quat, current_mat, Kpo
 
 def check_world_ee_contact_force(data, model, obj_name='board'):
     current_force_world = np.zeros(6)
+    current_force_local = np.zeros(6)
     contact_pos = None
     if data.ncon > 0:
         # Compute the contact forces.
@@ -159,8 +160,8 @@ def check_world_ee_contact_force(data, model, obj_name='board'):
                 break
         # Contact frame x-axis (normal) points FROM geom2 To geom1
         # from slope to ee
-        # contact_rot = contact.frame.reshape(3, 3) # from local to world
-        contact_rot = np.array([[0.0, 0.0, -1.0], [0.0, 1.0, 0.0], [1.0, -0.0, 0.0]])
+        contact_rot = contact.frame.reshape(3, 3).T # from local to world
+        contact_rot_local = np.array([[0.0, 1.0, 0.0], [0.0, 0.0, 1.0], [1.0, 0.0, 0.0]]) # move normal force from x to z
         contact_pos = contact.pos.copy()
         force_local = contact_force_local[:3]
         moment_local = contact_force_local[3:]
@@ -171,7 +172,10 @@ def check_world_ee_contact_force(data, model, obj_name='board'):
         moment_world = moment_rotated + position_cross_force
         current_force_world[:3] = force_world
         current_force_world[3:] = moment_world
-    return current_force_world, contact_pos
+        # local force
+        current_force_local[:3] = contact_rot_local @ force_local
+        current_force_local[3:] = contact_force_local[3:]
+    return current_force_world, current_force_local, contact_pos
 
 def dynamically_consistent_inv(jac, M_inv):
     """