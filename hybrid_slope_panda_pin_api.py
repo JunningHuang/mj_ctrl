fix rotation issue

hybrid_slope_panda_pin_api.py
@@ -16,7 +16,7 @@ from typing import Optional, Tuple
 from dataclasses import dataclass
 from enum import Enum
 
-from utils import *
+from utils_libfranka import *
 import matplotlib.pyplot as plt
 from geom_visualizer import visualize_normal_arrow, reset_scene
 from mujoco_robot_interface import MujocoRobotInterface, MujocoRobotState, Torques
@@ -353,7 +353,7 @@ class HybridController:
 
         self.R_slope = euler_to_rot_matrix(self.common_config.euler)
         rot_slope = Rotation.from_euler('xyz', self.common_config.euler)
-        self.quat_slope = rot_slope.as_quat(scalar_first=True)
+        self.quat_slope = np.roll(rot_slope.as_quat(),1)
 
         self.R = np.zeros((6, 6))
         self.R[0:3, 0:3] = self.R_slope
@@ -726,8 +726,8 @@ def main() -> None:
         # mujoco.mju_euler2Quat(quat_slope, common_config.euler, 'XYZ')
         # mujoco.mju_mulQuat(target_quat, quat_slope, target_quat)
         rot_slope = Rotation.from_euler('xyz', common_config.euler)
-        rot_target = Rotation.from_quat(target_quat, scalar_first=True)
-        target_quat = (rot_slope * rot_target).as_quat(scalar_first=True)
+        rot_target = Rotation.from_quat(np.roll(target_quat, -1))
+        target_quat = np.roll((rot_slope * rot_target).as_quat(), 1)
 
         # Start torque control
         print("\nStarting torque control...")

utils.py
@@ -257,7 +257,7 @@ def compute_ee_pose_error(target_pos, current_pos, target_quat, current_mat, Kpo
         rot_current = Rotation.from_matrix(np.eye(3))
     else:
         rot_current = Rotation.from_matrix(current_mat.reshape(3,3))
-    rot_target = Rotation.from_quat(target_quat, scalar_first=True)
+    rot_target = Rotation.from_quat(np.roll(target_quat, -1))
     
     R_error = rot_target * rot_current.inv()
     twist[3:] = R_error.as_rotvec()